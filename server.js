9d461aa788470b7b177eb6ea931c5f65270fc539
nahfilaali <nahfila.ali05@myhunter.cuny.edu>
2023-11-15 03:27:44 -0500

    nahfila commits for new log in/sign up stuff

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.html'));
+  res.sendFile(path.join(__dirname, 'homepage.html'));
 });
 
 app.listen(3000, () => {
