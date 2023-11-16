3154f211c244c265b7f1dcba76a23fadf43e5312
MarkBlinder <markblndr@gmail.com>
2023-11-16 16:49:22 -0500

    added basic task list

--- pomo/src/index.jsx
+++ pomo/src/index.jsx
@@ -1,32 +1,146 @@
 import { Canvas } from "@react-three/fiber";
-import "./style.css";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import Experience from "./Experience.jsx";
-import AnimationsContext from "./AnimationsContext";
 import * as THREE from "three";
-import React, { useState } from "react";
+import AnimationsContext from "./AnimationsContext";
+import Experience from "./Experience.jsx";
+import "./style.css";
+const api_base = "http://localhost:3001";
 
 const App = () => {
   const [playAllAnimations, setPlayAllAnimations] = useState(() => () => {});
 
-  return (
-    <AnimationsContext.Provider
-      value={{ playAllAnimations, setPlayAllAnimations }}
-    >
-      <button onClick={playAllAnimations}>Play Animations</button>
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
 
-      <Canvas
-        dpr={[1, 2]}
-        gl={{
-          antialias: true,
-          toneMapping: THREE.ACESFilmicToneMapping,
-          outputColorSpace: THREE.SRGBColorSpace,
-        }}
-        camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}
+  useEffect(() => {
+    GetTodos();
+  }, []);
+
+  const GetTodos = () => {
+    fetch(api_base + "/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((err) => console.error("Error: ", err));
+  };
+
+  const completeTodo = async (id) => {
+    const data = await fetch(api_base + "/todo/complete/" + id).then((res) =>
+      res.json()
+    );
+
+    setTodos((todos) =>
+      todos.map((todo) => {
+        if (todo._id === data._id) {
+          todo.complete = data.complete;
+        }
+
+        return todo;
+      })
+    );
+  };
+
+  const addTodo = async () => {
+    const data = await fetch(api_base + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: newTodo,
+      }),
+    }).then((res) => res.json());
+
+    setTodos([...todos, data]);
+
+    setPopupActive(false);
+    setNewTodo("");
+  };
+
+  const deleteTodo = async (id) => {
+    const data = await fetch(api_base + "/todo/delete/" + id, {
+      method: "DELETE",
+    }).then((res) => res.json());
+
+    setTodos((todos) => todos.filter((todo) => todo._id !== data.result._id));
+  };
+
+  return (
+    <div className="App">
+      <h1>Welcome, Mark</h1>
+      <h4>Your tasks</h4>
+
+      <div className="todos">
+        {todos.length > 0 ? (
+          todos.map((todo) => (
+            <div
+              className={"todo" + (todo.complete ? " is-complete" : "")}
+              key={todo._id}
+              onClick={() => completeTodo(todo._id)}
+            >
+              <div className="checkbox"></div>
+
+              <div className="text">{todo.text}</div>
+
+              <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
+                x
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>You currently have no tasks</p>
+        )}
+      </div>
+
+      <div className="addPopup" onClick={() => setPopupActive(true)}>
+        +
+      </div>
+
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            X
+          </div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input
+              type="text"
+              className="add-todo-input"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            <div className="button" onClick={addTodo}>
+              Create Task
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <AnimationsContext.Provider
+        value={{ playAllAnimations, setPlayAllAnimations }}
       >
-        <Experience />
-      </Canvas>
-    </AnimationsContext.Provider>
+        <button onClick={playAllAnimations}>Play Animations</button>
+
+        <Canvas
+          dpr={[1, 2]}
+          gl={{
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping,
+            outputColorSpace: THREE.SRGBColorSpace,
+          }}
+          camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}
+        >
+          <Experience />
+        </Canvas>
+      </AnimationsContext.Provider>
+    </div>
   );
 };
 
