18d5d54c8c677a8e7dcf70659939d26fd0da334f
karenwuxz <karenwuxz@gmail.com>
2023-12-05 20:19:24 -0500

    minor styling changes for login and signup pages

--- pomo/src/pages/login.jsx
+++ pomo/src/pages/login.jsx
@@ -1,37 +1,40 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import "./login.css";
 
 const Login = () => {
     return (
-        <div class="login-container">
-            <h1>Log In</h1>
-            <p>Not Registered Yet? <a href="signup.html">Create an account.</a></p>
-            <form id="login-form">
-                <div class="input-group">
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        placeholder="Username"
-                        required
-                    />
-                </div>
-                <div class="input-group">
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Password"
-                        required
-                    />
-                </div>
-                <div class="input-group">
-                    <a href="#">Forgot your password?</a>
-                </div>
-                <div class="input-group">
-                    <button type="submit">Log In</button>
-                </div>
-            </form>
+        <div class="login-content">
+            <div class="login-container">
+                <h1>Log In</h1>
+                <p>Not Registered Yet? <Link to="/signup">Create an account.</Link></p>
+                <form id="login-form">
+                    <div class="input-group">
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            placeholder="Username"
+                            required
+                        />
+                    </div>
+                    <div class="input-group">
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            required
+                        />
+                    </div>
+                    <div class="input-group">
+                        <a href="#">Forgot your password?</a>
+                    </div>
+                    <div class="input-group">
+                        <button type="submit">Log In</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- pomo/src/pages/signup.jsx
+++ pomo/src/pages/signup.jsx
@@ -1,34 +1,37 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import './login.css';
 
 const SignUp = () => {
     return(
-        <div class="login-container">
-            <h1>Sign Up</h1>
-            <p>Already a member? <a href="login.html">Log In.</a></p>
-            <form id="login-form">
-                <div class="input-group">
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        placeholder="Username"
-                        required
-                    />
-                </div>
-                <div class="input-group">
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Password"
-                        required
-                    />
-                </div>
-                <div class="input-group">
-                    <button type="submit">Sign Up</button>
-                </div>
-            </form>
+        <div class="login-content">
+            <div class="login-container">
+                <h1>Sign Up</h1>
+                <p>Already a member? <Link to="/login">Log In.</Link></p>
+                <form id="login-form">
+                    <div class="input-group">
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            placeholder="Username"
+                            required
+                        />
+                    </div>
+                    <div class="input-group">
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            required
+                        />
+                    </div>
+                    <div class="input-group">
+                        <button type="submit">Sign Up</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
