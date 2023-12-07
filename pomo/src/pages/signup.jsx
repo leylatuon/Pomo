0cb4ebd216d957543cac3ed838207b673ddcbeab
nahfilaali <nahfila.ali05@myhunter.cuny.edu>
2023-12-07 12:17:57 -0500

    .

--- pomo/src/pages/signup.jsx
+++ pomo/src/pages/signup.jsx
@@ -6,63 +6,57 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault(); // Prevent default form submission
+    const handleSubmit = async (event) => {
+        event.preventDefault(); 
 
-    // Send the username and password to the server
-    const response = await fetch("http://localhost:3001/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
+        const response = await fetch("http://localhost:3001/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+        
+        const data = await response.json();
+        if (response.ok) {
+            alert('User registered successfully');
+            navigate('/login'); 
+        } else {
+            alert(`Failed to register user: ${data.message}`);
+        }
+    };
 
-    const data = await response.json();
-    if (response.ok) {
-      alert("User registered successfully");
-      navigate("/login"); // Navigate to login page after successful registration
-    } else {
-      alert(`Failed to register user: ${data.message}`);
-    }
-  };
-
-  return (
-    <div className="login-content">
-      <div className="login-container">
-        <h1>Sign Up</h1>
-        <p>
-          Already a member? <a href="/login">Log In</a>
-        </p>{" "}
-        {/* Updated this line */}
-        <form id="login-form" onSubmit={handleSubmit}>
-          <div className="input-group">
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Username"
-              required
-            />
-          </div>
-          <div className="input-group">
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              required
-            />
-          </div>
-          <div className="input-group">
-            <button type="submit">Sign Up</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className="login-container">
+            <h1>Sign Up</h1>
+            <p>Already a member? <a href="/login">Log In</a></p> 
+            <form id="login-form" onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Username"
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <button type="submit">Sign Up</button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default SignUp;
